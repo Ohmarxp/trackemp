6ae586d152c4e5410e9f14e93c2d54df925e307f
Marxho <marxhop@hotmail.com>
2023-03-22 16:05:55 -0400

    Add index file and requests

---
+++ index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',       
+        password: '',
+        database: 'employee_db'
+    },   
+);
